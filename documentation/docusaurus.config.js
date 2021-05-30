e5ee8a0733a4350c66e6ffc3952f543a851ce730
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-05-30 09:49:28 +0530

    feat(documentation): create static documentation site

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,96 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Urban Dhobi",
+  tagline: "It's in the name ye dingus",
+  url: "https://github.com/thesidehustle/urban-dhobi.git",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "thesidehustle", // Usually your GitHub org/user name.
+  projectName: "urban-dhobi", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Urban Dhobi",
+      logo: {
+        alt: "Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        { to: "/docs/intro", label: "Documentation", position: "left" },
+        {
+          href: "https://github.com/thesidehustle/urban-dhobi.git",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
